4873ce526b20fbac518bfc10419fe806e4ce97de
Wxy <wangxiyu191@gmail.com>
2016-03-05 16:25:59 +0800

    change markdown render flags

--- reader/md/md.go
+++ reader/md/md.go
@@ -15,7 +15,25 @@ type MdArticle struct {
 }
 
 func md2html(input string) string {
-	output := blackfriday.MarkdownCommon([]byte(input))
+	renderFlag := 0 |
+		blackfriday.HTML_USE_XHTML |
+		blackfriday.HTML_USE_SMARTYPANTS |
+		blackfriday.HTML_SMARTYPANTS_FRACTIONS |
+		blackfriday.HTML_SMARTYPANTS_DASHES |
+		blackfriday.HTML_SMARTYPANTS_LATEX_DASHES
+	extensions := 0 |
+		blackfriday.EXTENSION_TABLES |
+		blackfriday.EXTENSION_FENCED_CODE |
+		blackfriday.EXTENSION_AUTOLINK |
+		blackfriday.EXTENSION_STRIKETHROUGH |
+		blackfriday.EXTENSION_SPACE_HEADERS |
+		blackfriday.EXTENSION_HEADER_IDS |
+		blackfriday.EXTENSION_BACKSLASH_LINE_BREAK |
+		blackfriday.EXTENSION_DEFINITION_LISTS |
+		blackfriday.EXTENSION_NO_EMPTY_LINE_BEFORE_BLOCK
+
+	htmlRenderer := blackfriday.HtmlRenderer(renderFlag, "", "")
+	output := blackfriday.Markdown([]byte(input), htmlRenderer, extensions)
 	return string(output)
 }
 
